ae8de38f5eed30a7d4ab2e384ef67e21b51180eb
kwartal <bv.barinov@gmail.com>
2022-08-13 22:17:48 +0300

    ending two VC

---
+++ LoginApp/LoginViewController.swift
@@ -0,0 +1,144 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by Богдан Баринов on 12.08.2022.
+//
+
+import UIKit
+
+final class LoginViewController: UIViewController {
+    
+    let loginTextField: UITextField = {
+        let textField = UITextField()
+        textField.textColor = .black
+        textField.font = .boldSystemFont(ofSize: 20)
+        textField.backgroundColor = .quaternarySystemFill
+        textField.placeholder = "Введите логин"
+        textField.textAlignment = .left
+        return textField
+    }()
+    
+    let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.textColor = .black
+        textField.font = .boldSystemFont(ofSize: 20)
+        textField.backgroundColor = .quaternarySystemFill
+        textField.placeholder = "Введите пароль"
+        textField.textAlignment = .left
+        textField.autocapitalizationType = .none
+        textField.isSecureTextEntry = true
+        
+        return textField
+    }()
+    
+    private let loginButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Log In", for: .normal)
+        button.tintColor = .red
+        button.backgroundColor = .white
+        button.setTitleColor(.link, for: .normal)
+        return button
+    }()
+    
+    private let forgotUserNameButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Forgot User Name?", for: .normal)
+        button.setTitleColor(.link, for: .normal)
+        button.titleLabel?.textAlignment = .center
+        button.titleLabel?.font = .systemFont(ofSize: 14)
+        return button
+    }()
+    
+    private let forgotPasswordButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Forgot User Name?", for: .normal)
+        button.setTitleColor(.link, for: .normal)
+        button.titleLabel?.textAlignment = .center
+        button.titleLabel?.font = .systemFont(ofSize: 14)
+        return button
+    }()
+    
+    // MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        title = "firstVC"
+        
+        addSubviews()
+        setupFrames()
+        
+        loginButton.addTarget(self, action: #selector(loginButtonDidTap), for: .touchUpInside)
+        
+        forgotUserNameButton.addTarget(self, action: #selector(forgotUserNameButtonPressed), for: .touchUpInside)
+        forgotPasswordButton.addTarget(self, action: #selector(forgotPasswordButtonPressed), for: .touchUpInside)
+    }
+
+
+    private func addSubviews() {
+        view.addSubview(loginTextField)
+        view.addSubview(passwordTextField)
+        view.addSubview(loginButton)
+        view.addSubview(forgotUserNameButton)
+        view.addSubview(forgotPasswordButton)
+    }
+    
+    private func setupFrames() {
+        loginTextField.frame = CGRect(x: 0, y: 300, width: 300, height: 30)
+        loginTextField.center.x = view.center.x
+        
+        passwordTextField.frame = CGRect(x: 0, y: loginTextField.frame.maxY + 50, width: 300, height: 30)
+        passwordTextField.center.x = view.center.x
+        
+        loginButton.frame = CGRect(x: 0, y: passwordTextField.frame.maxY + 40 , width: 60, height: 30)
+        loginButton.center.x = view.center.x
+        
+        forgotUserNameButton.frame = CGRect(x: 30, y: loginButton.frame.maxY + 40, width: 150, height: 30)
+        forgotPasswordButton.frame = CGRect(x: view.frame.maxX - 180, y: loginButton.frame.maxY + 40, width: 150, height: 30)
+        
+        
+    }
+    
+    @objc private func loginButtonDidTap() {
+//        if loginTextField.text == "Bogdan" && passwordTextField.text == "qwerty123" {
+        let vc = SecondViewController(login: "Welcome , \(loginTextField.text ?? "")")
+        vc.modalPresentationStyle = .fullScreen
+        present(vc, animated: true)
+
+//            navigationController?.pushViewController(vc, animated: true)
+//        vc.loginLabel.text = "Hi, \(loginTextField.text ?? "")"
+    
+//        } else {
+//            let alertController = UIAlertController(title: "Invalid login and password!", message: "Please, enter current login and password", preferredStyle: .alert)
+//            let action = UIAlertAction(title: "ok", style: .default)
+//            alertController.addAction(action)
+//            present(alertController, animated: true, completion: nil)
+//
+//        }
+        
+    }
+    
+    @objc private func forgotUserNameButtonPressed() {
+        let alertController = UIAlertController(title: "Oops!", message: "Your name is Bogdan", preferredStyle: .alert)
+        let action = UIAlertAction(title: "ok", style: .default)
+        alertController.addAction(action)
+        present(alertController, animated: true, completion: nil)
+        
+        
+    }
+    
+    @objc private func forgotPasswordButtonPressed() {
+        let alertController = UIAlertController(title: "Oops!", message: "Your password is qwerty123", preferredStyle: .alert)
+        let action = UIAlertAction(title: "ok", style: .default)
+        alertController.addAction(action)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super .touchesBegan(touches, with: event)
+    }
+    
+    
+}
+
---
+++ LoginApp/SecondViewController.swift
@@ -0,0 +1,99 @@
+//
+//  SecondViewController.swift
+//  LoginApp
+//
+//  Created by Богдан Баринов on 12.08.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+   
+    
+   private var loginLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.adjustsFontSizeToFitWidth = true
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 40)
+        return label
+    }()
+    
+    private var smileLabel: UILabel = {
+        let label = UILabel()
+        label.text = "👋🏽"
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 60)
+        return label
+    }()
+    
+    private var logOutButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Log out", for: .normal)
+        button.tintColor = .red
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .clear
+        button.titleLabel?.font = .systemFont(ofSize: 30)
+        return button
+    }()
+    
+    private var gradientLayer: CAGradientLayer = {
+        let layer = CAGradientLayer()
+        layer.colors = [UIColor.systemPink.cgColor,
+                        UIColor.systemPurple.cgColor,
+                        UIColor.systemBlue.cgColor]
+        return layer
+    }()
+    
+    init(login: String) {
+        super.init(nibName: nil, bundle: nil)
+        self.loginLabel.text = login
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+        addSubviews()
+        setupFrames()
+        
+        logOutButton.addTarget(self, action: #selector(backInLoginVC), for: .touchUpInside)
+  
+    }
+    
+    private func addSubviews() {
+        view.layer.addSublayer(gradientLayer)
+        view.addSubview(loginLabel)
+        view.addSubview(smileLabel)
+        view.addSubview(logOutButton)
+    }
+    
+    private func setupFrames() {
+        loginLabel.frame = CGRect(x: 0, y: 240, width: 200, height: 40)
+        loginLabel.center.x = view.center.x
+        
+        smileLabel.frame = CGRect(x: 0, y: loginLabel.frame.maxY + 30, width: 60, height: 60)
+        smileLabel.center.x = view.center.x
+        
+        logOutButton.frame = CGRect(x: 0, y: view.frame.maxY - 180, width: 100, height: 50)
+        logOutButton.center.x = view.center.x
+        
+        gradientLayer.frame = view.bounds
+        
+    }
+    
+    @objc func backInLoginVC() {
+        let vc = LoginViewController()
+        vc.modalPresentationStyle = .fullScreen
+        present(vc, animated: true)
+        
+//        vc.loginTextField.text = ""
+//        vc.passwordTextField.text = ""
+    }
+}
